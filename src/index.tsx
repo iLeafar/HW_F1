bed3cff23238e4a80507b343a07d494d292efb20
Rafael M <leafar077@gmail.com>
2022-11-10 21:18:12 +0300

    HW_F1

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import './css/index.css';
+import App from './components/App';
+
+const root = ReactDOMClient.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+
+root.render(
+    <App />
+);
